2c80782ecaa1aed7a972ade0b0c5f4e3ab50f670
--global <--global>
2022-09-21 09:37:17 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {Tabs, Tab, Sonnet} from 'react-bootstrap';
 
 function App() {
+  const [key, setKey] = useState('home');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      className="mb-3"
+    >
+      <Tab eventKey="home" title="Home">
+        <Sonnet />
+      </Tab>
+      <Tab eventKey="profile" title="Profile">
+        <Sonnet />
+      </Tab>
+      <Tab eventKey="contact" title="Contact" disabled>
+        <Sonnet />
+      </Tab>
+    </Tabs>
+      <span class="one">BED</span>
+      <span>침대</span>
+      
     </div>
   );
 }
